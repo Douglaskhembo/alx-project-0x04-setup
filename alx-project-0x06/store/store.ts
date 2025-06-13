d9a1d6d6d97d9926d7759274fda754e6afb29eac
khembo <khembo.douglas.juma@gmail.com>
2025-06-13 21:26:48 +0300

    Include a Redux for global state management

---
+++ alx-project-0x06/store/store.ts
@@ -0,0 +1,30 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      if (state.value > 0) {
+        state.value -= 1;
+      }
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
